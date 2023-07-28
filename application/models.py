430173813b58c697de602604d6ee34df7c6eca56
simran2002 <simran.kukreja714@gmail.com>
2023-07-28 16:30:44 +0530

    fixed

--- application/models.py
+++ application/models.py
@@ -1,7 +1,7 @@
-import email
-from unicodedata import name
-from main import db
+from flask_sqlalchemy import SQLAlchemy
 
+
+db = SQLAlchemy()
 class Venue(db.Model):
     __tablename__ = "venue"
     venue_id = db.Column(db.Integer, primary_key=True,autoincrement=True, nullable=False)
--- main.py
+++ main.py
@@ -1,19 +1,14 @@
+from typing import ByteString
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from application.models import db
 
-db = SQLAlchemy()
-
-def create_app():
-    app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///TicketShow_database.sqlite3"
-    app.config["DEBUG"] = False
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-    app.secret_key = "secretkey"
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///TicketShow_database.sqlite3"
+app.config["DEBUG"] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+app.secret_key = "secretkey"    
+db.init_app(app)
 
 if __name__ == "__main__":
-    db.create_all()
-    
-    app = create_app()
-    app.app_context().push()
-    db.init_app(app)
-    app.run()
+    app.run(host='0.0.0.0', port=8080, debug=True)
