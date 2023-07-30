dcdecbefc0fb7287a13109001bc17c601b6f310c
HY <harshy.167@gmail.com>
2023-07-30 13:49:28 +0530

    HEHE

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from typing import ByteString
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from application.models import db
+from models import db
+from flask_cors import CORS
+from views import api
+
 
 app = Flask(__name__)
+CORS(app)
+# cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///TicketShow_database.sqlite3"
 app.config["DEBUG"] = False
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-app.secret_key = "secretkey"    
+app.secret_key = "secretkey"
 db.init_app(app)
 
+app.app_context().push()
+
+api.init_app(app)
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8081, debug=True)
--- models.py
+++ models.py
@@ -1,6 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 
-
 db = SQLAlchemy()
 class Venue(db.Model):
     __tablename__ = "venue"
